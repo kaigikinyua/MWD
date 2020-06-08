75db68f1ab94df437bb688b8f05787f157cd9a36
kaigikinyua <kaigikinyua@gmail.com>
2020-06-08 16:47:50 +0300

    theme setting init

---
+++ static/js/UI.js
@@ -0,0 +1,38 @@
+var theme=""
+
+window.onload=()=>{
+    theme=localStorage.getItem("theme")
+    if(theme==null || theme==undefined){
+        localStorage.setItem("theme","light")
+        theme="light"
+    }
+    console.log(theme)
+    loadTheme();
+}
+
+function loadTheme(){
+    var elements=['.topnav','.landing','.player_form','.input','body']
+    elements.forEach(elem=>{
+        var e=document.querySelectorAll(elem)
+        e.forEach(elemClass=>{
+            elemClass.classList.add(theme)
+        });
+    });
+}   
+
+function switchTheme(){
+    var swithced_theme="";
+    if(theme=="light"){
+        swithced_theme="dark"
+    }else if(theme=="dark"){
+        swithced_theme="light"
+    }else{
+        swithced_theme="dark"
+    }
+    var elements=document.querySelectorAll("."+theme)
+    elements.forEach(elem=>{
+        elem.classList.remove(theme)
+        elem.classList.add(swithced_theme)
+    });
+    localStorage.setItem("theme",swithced_theme)
+}
